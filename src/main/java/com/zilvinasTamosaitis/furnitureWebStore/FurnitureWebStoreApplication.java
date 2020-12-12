f2f871a52e497e6ac4b0b15baaa68f99bdb8ea2c
Zilvinas06 <zilvinas.work@gmail.com>
2020-12-12 13:24:54 +0200

    First commit

---
+++ src/main/java/com/zilvinasTamosaitis/furnitureWebStore/FurnitureWebStoreApplication.java
@@ -0,0 +1,13 @@
+package com.zilvinasTamosaitis.furnitureWebStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurnitureWebStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurnitureWebStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zilvinasTamosaitis/furnitureWebStore/FurnitureWebStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zilvinasTamosaitis.furnitureWebStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurnitureWebStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
